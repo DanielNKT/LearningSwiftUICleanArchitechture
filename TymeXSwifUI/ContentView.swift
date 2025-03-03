63d8f6056ec4e80c210bf275e5f400aa142c4e58
nguyen toan <nktoan.dev@gmail.com>
2025-03-03 22:01:15 +0700

    Initial Commit

---
+++ TymeXSwifUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TymeXSwifUI
+//
+//  Created by Bé Gạo on 3/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TymeXSwifUI/TymeXSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TymeXSwifUIApp.swift
+//  TymeXSwifUI
+//
+//  Created by Bé Gạo on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TymeXSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
